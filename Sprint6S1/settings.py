a0202ce2b7c8ad299cd5f05c9fc7f0f349fb373b
zthornhill <70481420+zthornhill@users.noreply.github.com>
2021-03-28 21:56:37 -0500

    Update settings.py

--- Sprint6S1/settings.py
+++ Sprint6S1/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
